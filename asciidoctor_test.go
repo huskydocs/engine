efbbbcf04163b061bab771b9e3d4f8185408ea55
noamt <noam@10ne.org>
2016-01-29 00:55:05 +0200

    Provide command opts when testing package

--- asciidoctor_test.go
+++ asciidoctor_test.go
@@ -3,7 +3,19 @@ package engine
 import "testing"
 
 func TestInvoke(t *testing.T) {
-    err := Invoke(Opts())
+    opts := Opts()
+    opts.files = []string{"file1", "file2"}
+    opts.backend = "HTML5"
+    opts.doctype = "book"
+    opts.outFile = "targetFile"
+    opts.noHeaderFooter = true
+    opts.sectionNumbers = true
+    opts.attributes = make(map[string]string)
+    opts.attributes["att1"] = "value1"
+    opts.attributes["att2"] = "value2"
+    opts.baseDir = "baseDir"
+    opts.destinationDir = "destinationDir"
+    err := Invoke(opts)
     if err != nil {
         t.Errorf("Unexpected Exec error %s", err)
     }
